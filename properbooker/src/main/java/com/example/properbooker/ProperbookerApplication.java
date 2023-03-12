c4c312c8f439459235aabc56437e094a0d30565c
Nikola <nikanberko286@gmail.com>
2023-03-12 22:03:04 +0100

    deleted old project files, added new

--- properbooker/src/main/java/com/example/properbooker/ProperbookerApplication.java
+++ properbooker/src/main/java/com/example/properbooker/ProperbookerApplication.java
@@ -1,4 +1,4 @@
-package com.properbooker;
+package com.example.properbooker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProperbookerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProperbookerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProperbookerApplication.class, args);
+    }
 
 }
--- properbooker/src/test/java/com/example/properbooker/ProperbookerApplicationTests.java
+++ properbooker/src/test/java/com/example/properbooker/ProperbookerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.properbooker;
+package com.example.properbooker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProperbookerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
