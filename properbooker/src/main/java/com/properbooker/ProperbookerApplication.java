82ebc564c578a757bf9329464ccf64b663677740
Nikola <nikanberko286@gmail.com>
2023-03-12 20:17:45 +0100

    Initialize project files

---
+++ properbooker/src/main/java/com/properbooker/ProperbookerApplication.java
@@ -0,0 +1,13 @@
+package com.properbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProperbookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProperbookerApplication.class, args);
+	}
+
+}
---
+++ properbooker/src/test/java/com/properbooker/ProperbookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.properbooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProperbookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
